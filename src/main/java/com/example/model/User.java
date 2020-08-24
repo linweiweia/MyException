51146f9a9af6c6d7dc17f130f4f69558ee0c8ff5
lww <1013232063@qq.com>
2020-08-24 18:16:20 +0800

    first commit

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+/**
+ * @author linweiwei
+ * @version 1.0
+ * @date 2020-08-24 17:48
+ * @describe:
+ */
+public class User {
+
+    @NotNull(message = "用户Id不能为空")
+    private Long id;
+
+    @NotNull(message = "用户账号不能为空")
+    @Size(min = 6, max = 12, message = "用户名长度6到12")
+    private String account;
+
+    @NotNull(message = "用户密码不能为空")
+    @Size(min = 6, max = 12, message = "用户密码长度6到12")
+    private String password;
+
+    @NotNull(message = "用户邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+
+}
